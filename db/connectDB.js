784d3e7ef965f51d1dcfdb5521316f67a947cc7a
sewhail <suhailshahul8606@gmail.com>
2024-01-07 18:22:34 +0530

    added db connection

---
+++ db/connectDB.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import ora from 'ora'
+import chalk from 'chalk'
+
+
+dotenv.config()
+
+export async function connectDB() {
+    try {
+        const spinner = ora('connecting to the database...').start()
+        await mongoose.connect(process.env.MONGO_URI)
+        spinner.stop()
+        console.log(chalk.greenBright('successfully connected to the database!!!'))
+    } catch (error) {
+        console.log(chalk.redBright('error : '), error)
+        process.exit(1)
+    }
+}
+
+export async function disconnectDB() {
+    try {
+        await mongoose.disconnect()
+        console.log(chalk.greenBright('successfully disconnected from the database'))
+    } catch (error) {
+        console.log(chalk.redBright('error : '), error)
+        process.exit(1)
+    }
+}
